b5bf1556249010dfce3fa8d3e31f4a3bea516a3c
Meticulus <theonejohnnyd@gmail.com>
2014-11-06 00:58:05 +0100

    STE OMX Fix Fix Camera, Camcorder and Torch on STE Devices
    Change-Id: I46d1a04a8fcc63546b319d31d9299517406e783a

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3445,6 +3445,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
     uint32_t code;
     Parcel const* data;
     Parcel* reply;
+    Mutex mLock;
 
     enum {
         MSG_API_CALL,
@@ -3458,6 +3459,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
      */
     virtual status_t transact(uint32_t code,
             const Parcel& data, Parcel* reply, uint32_t /* flags */) {
+        mLock.lock();
         this->code = code;
         this->data = &data;
         this->reply = reply;
@@ -3475,6 +3477,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
             looper->sendMessage(this, Message(MSG_API_CALL));
             barrier.wait();
         }
+        mLock.unlock();
         return result;
     }
 
